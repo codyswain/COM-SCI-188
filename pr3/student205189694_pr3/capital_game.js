f77a3ff9bba5d3101b6034f4674ff54a0fa04b0b
Cody Swain <swain.cody@gmail.com>
2018-11-14 17:27:20 -0800

    Working map

--- pr3/student205189694_pr3/capital_game.js
+++ pr3/student205189694_pr3/capital_game.js
@@ -1,6 +1,17 @@
 // This allows the Javascript code inside this block to only run when the page
 // has finished loading in the browser.
 
+
+//Google maps initialization
+var map;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+}
+
+
 $( document ).ready(function() {
 
 	// Initialize Cloud Firestore through Firebase
